c22c426e4597c67d73bc29d7125cd956cc6c8722
Antonio Silva <a.silva@pointerbp.nl>
2018-01-16 11:39:11 +0100

    sample4 using v2 data

--- tutorial/sample4.py
+++ tutorial/sample4.py
@@ -14,12 +14,12 @@ k = 1
 delimiter = ","
 
 # load features names
-names = pd.read_csv('data/listings/v1/listings_feature_names.data', sep=delimiter, header=None)
+names = pd.read_csv('data/listings/v2/listings_feature_names.data', sep=delimiter, header=None)
 print(names.values)
 
 # load data
-X = np.loadtxt('data/listings/v1/listings.data', delimiter=delimiter)
-y = np.loadtxt('data/listings/v1/listings_target.data', delimiter=delimiter)
+X = np.loadtxt('data/listings/v2/listings.data', delimiter=delimiter)
+y = np.loadtxt('data/listings/v2/listings_target.data', delimiter=delimiter)
 
 # instance of knn
 
@@ -30,8 +30,8 @@ knn.fit(X, y)
 # predict
 a = knn.predict(
     [
-        [1, 1, 1, 3, 276, 8, 276, 1, 54.9, 4, 0, 0],
-        [1, 2, 2, 1, 840, 9, 840, 1, 130.02, 14, 0, 1]
+        [1, 1, 1, 3, 276, 8, 276, 1, 54.9, 4, 0],
+        [1, 2, 2, 1, 840, 9, 840, 1, 130.02, 14, 0]
     ]
 )
 print(a)
