a4d737ad78126a2d7426ff18bb5ba4fa6abeab83
Antonio Silva <a.silva@pointerbp.nl>
2018-01-16 13:41:05 +0100

    clean up sample4

--- tutorial/sample4.py
+++ tutorial/sample4.py
@@ -7,20 +7,19 @@ import pandas as pd
 # 2 - OK
 # 3 - Relevant (escalated or infriging)
 
-# n_neighbors value
-k = 1
+# variables
+n_neighbors = 1
 delimiter = ","
 
 # load features names
 names = pd.read_csv('data/listings/v2/features_names.csv', sep=delimiter, header=None)
-#print(names.values)
 
 # load data
 X1 = np.loadtxt('data/listings/v2/listings.csv', delimiter=delimiter)
 y1 = np.loadtxt('data/listings/v2/listings_target.csv', delimiter=delimiter)
 
 # instance of knn
-knn = KNeighborsClassifier(n_neighbors=k)
+knn = KNeighborsClassifier(n_neighbors=n_neighbors)
 
 # fit data to knn
 knn.fit(X1, y1)
@@ -30,15 +29,13 @@ y2 = np.loadtxt('data/listings/v2/listings_test_target.csv', delimiter=delimiter
 
 # predict
 prediction = knn.predict(X2)
-#print(prediction)
 
+# check results
 wrong = 0
 for index in range(len(prediction)):
-    #print(y2[index])
-    #print(prediction[index])
-    #print(y2[index]==prediction[index])
     if y2[index] != prediction[index]:
         wrong += 1
 
+# Accuracy
 accuracy = (len(prediction)-wrong)/len(prediction)
 print('Accuracy ' + str(accuracy*100) + '%')
